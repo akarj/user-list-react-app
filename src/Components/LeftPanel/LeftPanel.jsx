f6f9286b2de906da3218532a16f93c9fe0ff6e30
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-10-24 22:41:29 +0530

    Left side-Panel Text UI Structured!!

--- src/Components/LeftPanel/LeftPanel.jsx
+++ src/Components/LeftPanel/LeftPanel.jsx
@@ -1,5 +1,16 @@
 import "./LeftPanel.scss";
 
 export default function LeftPanel() {
-  return <div className="Left-Panel-Container">Left Panel</div>;
+  return (
+    <div className="Left-Panel-Container">
+      <section>
+        <header>
+          User
+          <br />
+          List <br />
+          Application
+        </header>
+      </section>
+    </div>
+  );
 }
